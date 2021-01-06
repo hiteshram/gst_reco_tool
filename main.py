f00f7d94748c1f89647a3e06c626bc85b61e5edb
Hitesh Ram <hiteshram.kotha@gmail.com>
2021-01-06 15:52:35 +0530

    Aggregations of the results completed

--- main.py
+++ main.py
@@ -36,25 +36,39 @@ for index, row in accounts_df.iterrows():
     company_gst_master[row['Sales Tax No.']]=row['Particulars']
 
 
-temp_file_path=os.path.join(cwd,"temp","gst_reco.csv")
+reco_file_one_path=os.path.join(cwd,"temp","gst_reco_completed.csv")
+reco_file_two_path=os.path.join(cwd,"temp","gst_reco.csv")
 
-if os.path.exists(temp_file_path):
-    os.remove(temp_file_path)
+if os.path.exists(reco_file_one_path) and os.path.exists(reco_file_two_path):
+    os.remove(reco_file_one_path)
+    os.remove(reco_file_two_path)
 
 for key,value in company_gst_master.items():
-    company_df=pd.DataFrame([key,value]).transpose()
-    company_df.to_csv(temp_file_path, mode='a',header=False,index=False)
-    temp_df=pd.DataFrame(["Input","Date","Bill Number","Total"]).transpose()
-    temp_df.to_csv(temp_file_path, mode='a',header=False,index=False)
-
-    account_temp_df=accounts_df.loc[accounts_df['Sales Tax No.'] == key]
-    for index,row in account_temp_df.iterrows():
-        temp_df=pd.DataFrame(["Tally", row["Date"], row["Narration"],row["Gross Total"]]).transpose()
-        temp_df.to_csv(temp_file_path, mode='a',header=False,index=False)
     
-    gstr_temp_df=gstr_df.loc[gstr_df['GSTIN of supplier']==key]
-    for index,row in gstr_temp_df.iterrows():
-        temp_df=pd.DataFrame(["GST Input",row["Invoice Date"], row["Invoice number"],row["Invoice Value"]]).transpose()
-        temp_df.to_csv(temp_file_path, mode='a',header=False,index=False)
+    company_df=pd.DataFrame([key,value]).transpose()
+    account_temp_df=accounts_df.loc[accounts_df['Sales Tax No.'] == key]
+    acc_df=pd.DataFrame(columns=["Source","Date","Narration","Gross Total"])
+    
+    for index,row in account_temp_df.iterrows():       
+        temp_df = {"Source": "Books", 'Date': row["Date"],"Narration":row["Narration"][8:].strip(),"Gross Total":row["Gross Total"]}
+        acc_df=acc_df.append(temp_df,ignore_index=True)
 
-os.startfile(temp_file_path)
+    gstr_temp_df=gstr_df.loc[gstr_df['GSTIN of supplier']==key]
+    gst_df=pd.DataFrame(columns=["GST Source","Invoice Date","Invoice number","Invoice Value"])
+    for index,row in gstr_temp_df.iterrows():
+        row["Invoice number"]=row["Invoice number"].strip()
+        temp_df={"GST Source":"GSTR2B Input","Invoice Date":row["Invoice Date"],"Invoice number":row["Invoice number"],"Invoice Value":row["Invoice Value"]}
+        gst_df=gst_df.append(temp_df,ignore_index=True)
+    
+    acc_gst_merge=pd.merge(acc_df,gst_df,left_on="Narration",right_on="Invoice number",how="outer")
+
+    if acc_gst_merge["Source"].isnull().values.any() or acc_gst_merge["GST Source"].isnull().values.any(): 
+        company_df.to_csv(reco_file_two_path, mode='a',header=False,index=False)
+        acc_gst_merge.to_csv(reco_file_two_path, mode='a',index=False) 
+    else:
+        company_df.to_csv(reco_file_one_path, mode='a',header=False,index=False)
+        acc_gst_merge.to_csv(reco_file_one_path, mode='a',index=False)
+
+
+os.startfile(reco_file_one_path)
+os.startfile(reco_file_two_path)
